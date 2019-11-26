4d8a508226b4d2cb775e364dc9d8eea899336e00
Thao Le <thao.le@s3corp.com.vn>
2019-11-26 17:46:12 +0700

    fix contex

--- App.js
+++ App.js
@@ -5,8 +5,23 @@ import ContextProvider from './ContextProvider/ContextProvider';
 import Context from './context/Context';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.renderButtons = this.renderButtons.bind(this);
+  }
 
-  render() {
+  renderButtons(handleInput) {
+    let layouts = buttons.map((buttonRow, rowIndex) => {
+      let rowItem = buttonRow.map((buttonItem, buttonIndex) => {
+        return <InputNumberButton value={buttonItem} handleOnPress={() => handleInput(buttonItem)} key={'btn-' + buttonIndex} />
+      })
+      return <View style={styles.inputRow} key={'row-' + rowIndex}>{rowItem}</View>
+    })
+
+    return layouts; 
+  }
+
+  render() {    
     return (
       <ContextProvider>
         <Context.Consumer>
@@ -16,7 +31,7 @@ export default class App extends React.Component {
                 <Text style={styles.inputText}>{data.state.displayValue}</Text>
               </View> 
               <View style={styles.inputContainer}>
-                {data.renderButtons}
+                {this.renderButtons(data.handleInput)}
               </View>
             </View>
           )}
@@ -26,6 +41,14 @@ export default class App extends React.Component {
     );
   }
 }
+
+const buttons = [
+  ['CLEAR', 'DEL'],
+  ['7', '8', '9', '/'],
+  ['4', '5', '6', 'x'],
+  ['1', '2', '3', '-'],
+  ['0', '.', '=', '+']
+];
 
 const styles = StyleSheet.create({
   container: {
@@ -46,5 +69,9 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     padding: 20,
     textAlign: 'right'
+  },
+  inputRow: {
+    flex: 1,
+    flexDirection: 'row',    
   }
 });
--- ContextProvider/ContextProvider.js
+++ ContextProvider/ContextProvider.js
@@ -13,104 +13,93 @@ export default class ContextProvider extends React.Component {
           secondValue: '',
           nextValue: false
         }
-        this.state = this.initialState
+        this.state = this.initialState;
     }
 
-    renderButtons = () => {
-        let layouts = buttons.map((buttonRow, rowIndex) => {
-          let rowItem = buttonRow.map((buttonItem, buttonIndex) => {
-            return <InputNumberButton value={buttonItem} handleOnPress={this.handleInput.bind(this, buttonItem)} key={'btn-' + buttonIndex} />
-          })
-          return <View style={styles.inputRow} key={'row-' + rowIndex}>{rowItem}</View>
-        })
-    
-        return layouts;
+    handleInput = input => {  
+      const { displayValue, operator, firstValue, secondValue, nextValue } = this.state;
+  
+      switch(input) {
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8': 
+        case '9':
+          this.setState({
+            displayValue: (displayValue === '0') ? input : displayValue + input
+          });
+          if (!nextValue) {
+            this.setState({
+              firstValue: firstValue + input
+            })
+          } else {
+            this.setState({
+              secondValue: secondValue + input
+            })
+          }
+          break;
+        case '+':
+        case '-':
+        case 'x':
+        case '/':
+          this.setState({
+            nextValue: true,
+            operator: input,
+            displayValue: (operator !== null ? displayValue.substr(0, displayValue.length -1) : displayValue) + input
+          }); 
+          break;       
+        case '.':
+          let dot = displayValue.toString().slice(-1);
+          this.setState({
+            displayValue: dot !== '.' ? displayValue + input : displayValue
+          }); 
+          if (!nextValue) {
+            this.setState({
+              firstValue: firstValue + input
+            })
+          } else {
+            this.setState({
+              secondValue: secondValue + input
+            })
+          }
+          break;  
+        case 'CLEAR': 
+          this.setState(this.initialState);
+          break;
+        case 'DEL':
+          let string = displayValue.toString();
+          let deletedString = string.substr(0, string.length - 1);
+          let length = string.length;
+          this.setState({
+            displayValue: length == 1 ? '0' : deletedString,
+            firstValue: length == 1 ? '0' : deletedString
+          });
+          break;    
+        case '=':
+          let formatOperator = operator == "x" ? "*" : operator;
+          let result = eval(firstValue + formatOperator + secondValue);
+          this.setState({
+            displayValue: result % 1 === 0 ? result : result.toFixed(2),
+            firstValue: result % 1 === 0 ? result : result.toFixed(2),
+            secondValue: '',
+            nextValue: false,
+            operator: null
+          });
+          break;  
+      } 
     }
 
-    handleInput = input => {
-        const { displayValue, operator, firstValue, secondValue, nextValue } = this.state;
-    
-        switch(input) {
-          case '0':
-          case '1':
-          case '2':
-          case '3':
-          case '4':
-          case '5':
-          case '6':
-          case '7':
-          case '8':
-          case '9':
-            this.setState({
-              displayValue: (displayValue === '0') ? input : displayValue + input
-            });
-            if (!nextValue) {
-              this.setState({
-                firstValue: firstValue + input
-              })
-            } else {
-              this.setState({
-                secondValue: secondValue + input
-              })
-            }
-            break;
-          case '+':
-          case '-':
-          case 'x':
-          case '/':
-            this.setState({
-              nextValue: true,
-              operator: input,
-              displayValue: (operator !== null ? displayValue.substr(0, displayValue.length -1) : displayValue) + input
-            }); 
-            break;       
-          case '.':
-            let dot = displayValue.toString().slice(-1);
-            this.setState({
-              displayValue: dot !== '.' ? displayValue + input : displayValue
-            }); 
-            if (!nextValue) {
-              this.setState({
-                firstValue: firstValue + input
-              })
-            } else {
-              this.setState({
-                secondValue: secondValue + input
-              })
-            }
-            break;  
-          case 'CLEAR': 
-            this.setState(this.initialState);
-            break;
-          case 'DEL':
-            let string = displayValue.toString();
-            let deletedString = string.substr(0, string.length - 1);
-            let length = string.length;
-            this.setState({
-              displayValue: length == 1 ? '0' : deletedString,
-              firstValue: length == 1 ? '0' : deletedString
-            });
-            break;    
-          case '=':
-            let formatOperator = operator == "x" ? "*" : operator;
-            let result = eval(firstValue + formatOperator + secondValue);
-            this.setState({
-              displayValue: result % 1 === 0 ? result : result.toFixed(2),
-              firstValue: result % 1 === 0 ? result : result.toFixed(2),
-              secondValue: '',
-              nextValue: false,
-              operator: null
-            });
-            break;  
-        } 
-    }
-    
     render() {
         return(
             <Context.Provider value={{ 
                 data: {
                     state: this.state,
-                    renderButtons: this.renderButtons()
+                    handleInput: this.handleInput
                 }
             }}>
                 {this.props.children}
@@ -119,17 +108,5 @@ export default class ContextProvider extends React.Component {
     }
 };
 
-const buttons = [
-    ['CLEAR', 'DEL'],
-    ['7', '8', '9', '/'],
-    ['4', '5', '6', 'x'],
-    ['1', '2', '3', '-'],
-    ['0', '.', '=', '+']
-];
 
-const styles = StyleSheet.create({
-  inputRow: {
-    flex: 1,
-    flexDirection: 'row',    
-  }
-})
+
